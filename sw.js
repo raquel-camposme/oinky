e1488dcf29123a522966fb68482860fcb1755b82
Raquel Silva Campos <raquelcampos@Raquels-MacBook-Pro.local>
2026-07-01 18:46:16 +0200

    Initial commit: Oinky house education app

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'oinky-v2';
+const ASSETS = ['/', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/pig-jar.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/')))
+  );
+});
